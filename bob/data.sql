b6a46164a834d7c76ec0457d2dc955315c135eb2
Varun Sapre <varun.sapre@gmail.com>
2021-06-06 16:24:39 +0530

    update database table schema

--- bob/data.sql
+++ bob/data.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE Bob;
-CREATE TABLE `Bob`.`User` (
+CREATE DATABASE bob;
+CREATE TABLE `bob`.`User` (
   `user_id` BIGINT UNIQUE AUTO_INCREMENT,
   `fname` VARCHAR(45) NULL,
   `lname` VARCHAR(45) NULL,
   `email` VARCHAR(45) NULL,
-  `password` VARCHAR(100) NULL,
+  `password` VARCHAR(110) NULL,
   PRIMARY KEY (`user_id`));
